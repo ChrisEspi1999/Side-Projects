de674f9a3810ea9269394b361393ac2746b1a7ac
Chris Espinosa <99517753+ChrisEspi1999@users.noreply.github.com>
2022-07-25 15:09:52 -0500

    Update

---
+++ Flappy Bird/script.js
@@ -0,0 +1,41 @@
+var pipe = document.getElementById("pipe");
+var gap = document.getElementById("gap");
+var player = document.getElementById("player");
+var jumping = 0;
+var counter = 0;
+
+gap.addEventListener('animationiteration', () => {
+    var random = -((Math.random()*300)+150);
+    gap.style.top = random + "px";
+    counter++;
+});
+
+setInterval(function(){
+    var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    if(jumping == 0){
+    player.style.top = (playerTop+3)+"px";
+    }
+    if(playerTop>480){
+        alert("You Died! Score: " + (counter-1));
+        player.style.top = 100 + "px";
+        counter = 0;
+    }
+},10);
+
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var playerTop = 
+        parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+        if((playerTop>6) && (jumpCount<15)){
+        player.style.top = (playerTop-5)+"px";
+    }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping = 0;
+            jumpCount = 0;
+        }
+        jumpCount++;
+    },10);
+}
