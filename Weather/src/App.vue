b3b31760ac4262f8ca1f391158529907435b13f1
Chris Espinosa <99517753+ChrisEspi1999@users.noreply.github.com>
2022-08-26 09:48:19 -0500

    Weather App

---
+++ Weather/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+export default{
+  name: 'app',
+  data(){
+    return{
+      api_key: 'bd0f743b23963dc4ab289dace43d6a63'
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..."/>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
+  }
+  body{
+    font-family: 'montserrat', sans-serif;
+  }
+  #app{
+    background-image: url('D:\Local Repository\Weather\src\assets\cold-bg.jpg');
+  }
+</style>
